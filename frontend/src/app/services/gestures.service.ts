528b5047a41dfeccc59551bdce021da8bc8b4592
macman1717 <mchawk1717@gmail.com>
2025-03-15 15:30:10 -0400

    added api calls to service for audio and fixed gesture calls

--- frontend/src/app/services/gestures.service.ts
+++ frontend/src/app/services/gestures.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {Audio, Gesture} from "../models/models";
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +12,17 @@ export class GesturesService {
   constructor(private http: HttpClient) {
   }
 
-  public register(username: string, password: string) {
-    console.log(username, password, this.apiUrl);
-    return this.http.post(
-      `${this.apiUrl}accounts/create`,
-      {"username": username, "password": password}
-    )
-  }
-
   public addGesture(username : string, audio_file: any, gesture : string){
     var fd = new FormData()
     fd.append('audio',audio_file)
     fd.append('gesture', gesture)
-    return this.http.post<string>(
+    return this.http.post<Gesture>(
       `${this.apiUrl}gesture/${username}/create`, fd
     )
   }
 
-  public getGesture(username : string, gesture : string){
-    return this.http.get<string>(
+  public getGestures(username : string, gesture : string){
+    return this.http.get<Gesture[]>(
       `${this.apiUrl}gesture/${username}/${gesture}`
     )
   }
@@ -37,6 +30,27 @@ export class GesturesService {
   public deleteGesture(username : string, gesture : string){
     return this.http.delete(
       `${this.apiUrl}gesture/${username}/${gesture}/delete`
+    )
+  }
+
+  public addAudio(username : string,audio_name : string, audio_file: any){
+    var fd = new FormData()
+    fd.append('audio',audio_file)
+    fd.append('audio_name', audio_name)
+    return this.http.post<Audio>(
+      `${this.apiUrl}gesture/audio/${username}/create`, fd
+    )
+  }
+
+  public getAudios(username : string) {
+    return this.http.get<Audio[]>(
+        `${this.apiUrl}gesture/audio/${username}`
+    )
+  }
+
+  public deleteAudio(username : string, audio_name : string){
+    return this.http.delete(
+        `${this.apiUrl}gesture/audio/${username}/${audio_name}/delete`
     )
   }
 }
