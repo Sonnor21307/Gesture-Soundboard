fd98a870fd35a3829738aee32d8acf9814bb86cb
Sean Nolan <seannolan21307@gmail.com>
2025-03-16 01:11:53 -0400

    revert api url

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class AuthService {
-  apiUrl = "http://127.0.0.1:8000/";
+  apiUrl = "https://gesture-soundboard.vercel.app/";
   constructor(private http: HttpClient) {}
 
   public logout() {
